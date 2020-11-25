e9695d2d5b7ad95c645f264e0d0591b7e2fe5666
melsimpson1023@gmail.com <L0v3ch1ld>
2020-11-25 16:55:02 -0500

    Created the create script
    modified these files to add create.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = ''
+const mongooseBaseName = 'Dev-Sweater-Back'
 
 // create the mongodb uri for development and test
 const database = {
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const productRoutes = require('./app/routes/product_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -64,6 +65,7 @@ app.use(requestLogger)
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(productRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
